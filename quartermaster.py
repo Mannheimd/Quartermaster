0836bed00cb46734156885c190cb5019b3251542
Matthew Parnell <matt@parnmatt.co.uk>
2018-01-29 18:01:35 +0000

    Refactor message logging into client
    The standard syntax to send a message is `client.send_message`.
    This moves the logging functionality of the free function `send_message`
    into a new specialised Client class.

--- quartermaster.py
+++ quartermaster.py
@@ -9,8 +9,20 @@ import os
 import discord
 
 
+class Client(discord.Client):
+    """Quatermaster client."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def send_message(self, destination, content=None, *, tts=False, embed=None):
+        if content is not None:
+            print(time_now(), '- Sending message to', f'#{destination}:', content)
+        await super().send_message(destination, content, tts=tts, embed=embed)
+
+
 # module level client
-client = discord.Client()
+client = Client()
 
 
 def time_now():
@@ -28,11 +40,6 @@ async def on_message(message):
         command = message.content.split(' ', 1)[0]
         print(time_now(), '- User: ', message.author.name, f'({message.author.id})', 'sent:', message.content)
         await command_center(command, message)
-
-
-async def send_message(channel, message):
-    print(time_now(), '- Sending message to', f'{channel.name}:', message)
-    await client.send_message(channel, message)
 
 
 async def command_center(command, message):
@@ -54,21 +61,21 @@ async def command_center(command, message):
 
 async def run_shutdown(message):
     if message.author.server_permissions.administrator:
-        await send_message(message.channel, 'Goodbye')
+        await client.send_message(message.channel, 'Goodbye')
         raise KeyboardInterrupt()
     else:
-        await send_message(message.channel, f"Sorry, I can't let you do that {message.author.mention}.")
+        await client.send_message(message.channel, f"Sorry, I can't let you do that {message.author.mention}.")
 
 
 async def run_hello(message):
-    await send_message(message.channel, f'Hello to you too, {message.author.mention}!')
+    await client.send_message(message.channel, f'Hello to you too, {message.author.mention}!')
 
 
 async def run_myroles(message):
     roles_textified = 'Your roles:'
     for role in message.author.roles:
         roles_textified = roles_textified + '\n' + role.name.strip('@')
-    await send_message(message.channel, roles_textified)
+    await client.send_message(message.channel, roles_textified)
 
 
 async def run_amiadmin(message):
@@ -76,31 +83,32 @@ async def run_amiadmin(message):
     if message.author.server_permissions.administrator:
         msg = f'Indeed {message.author.mention}, you *are* an admin'
 
-    await send_message(message.channel, msg)
+    await client.send_message(message.channel, msg)
 
 
 async def run_lightthebeacons(message):
     try:
         argument = message.content.split(' ', 1)[1].strip('@')
     except:
-        await send_message(message.channel,
-                           f"I'm sorry {message.author.mention}, I couldn't see a valid role. "
-                           'To light the beacons, use `?lightthebeacons RoleName` without the @ sign on the role. '
-                           'Example: `?lightthebeacons Overwatchers`')
+        await client.send_message(
+                message.channel,
+                f"I'm sorry {message.author.mention}, I couldn't see a valid role. "
+                'To light the beacons, use `?lightthebeacons RoleName` without the @ sign on the role. '
+                'Example: `?lightthebeacons Overwatchers`')
         return
 
     for role in message.server.roles:
         if role.name.lower() == argument.lower():
             if role.mentionable:
                 await client.delete_message(message)
-                await send_message(message.channel,
+                await client.send_message(message.channel,
                                    f"The beacons are lit! {role.mention}, will you come to {message.author.mention}'s aid?")
                 return
             else:
-                await send_message(message.channel, f"I'm sorry {message.author.mention}, that role can't be @mentioned.")
+                await client.send_message(message.channel, f"I'm sorry {message.author.mention}, that role can't be @mentioned.")
                 return
 
-    await send_message(message.channel, f"I'm sorry {message.author.mention}, I can't find a role called '{argument}'.")
+    await client.send_message(message.channel, f"I'm sorry {message.author.mention}, I can't find a role called '{argument}'.")
 
 
 def run(*args):
