47f0be1cd3be46b9dfe4f2f719eefc44170cc9f1
Matthew Parnell <matt@parnmatt.co.uk>
2018-02-05 21:27:32 +0000

    Alphabetise utils

--- utils.py
+++ utils.py
@@ -3,9 +3,24 @@
 import itertools as it
 
 
+def find(pred, iterable):
+    """
+    Find the first occurrence of the predicate function returning true
+    over the iterable; otherwise None.
+    >>> find(lambda e: e.startswith('g'), ['alpha', 'beta', 'gamma', 'delta'])
+    'gamma'
+    >>> find(lambda e: e.startswith('p'), ['alpha', 'beta', 'gamma', 'delta'])
+    None
+    """
+
+    for element in iterable:
+        if pred(element):
+            return element
+
+
 def flatten(iter_of_iters, fillvalue=None):
     """
-    Flatten one level of nesting.
+    Flatten one level of nesting. Returns a generator.
     [ [A, B], [C, D], [E, F, G] ] ==> [A, B, C, D, E, F, G]
 
     in the case of `fillvalue is not None`
@@ -24,15 +39,3 @@ def flatten(iter_of_iters, fillvalue=None):
                 yield element
 
 
-def find(pred, iterable):
-    """
-    Find the first occurrence of the predicate function returning true over the iterable; otherwise None.
-    >>> find(lambda e: e.startswith('g'), ['alpha', 'beta', 'gamma', 'delta'])
-    'gamma'
-    >>> find(lambda e: e.startswith('p'), ['alpha', 'beta', 'gamma', 'delta'])
-    None
-    """
-
-    for element in iterable:
-        if pred(element):
-            return element
